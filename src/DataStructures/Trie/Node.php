e7a66e6b4df201a2983e80c34a28e782aa5bcfd8
Lloyd Saldanha <lloydsaldanha@gmail.com>
2020-12-26 18:38:42 -0500

    Added Comments, Explanations for Trie

--- src/DataStructures/Trie/Node.php
+++ src/DataStructures/Trie/Node.php
@@ -1,6 +1,14 @@
 <?php
 namespace App\DataStructures\Trie;
 
+/**
+ * Explanation:
+ * ----------------------
+ * The Node class will represent a single alphabet in the Trie ( or each node )
+ * It contains a hash map of child nodes to indicate there are more chars forming a word after the current node
+ * the `isTerminal` flag will indicate if the current node is a complete word. It does not mean that there are no
+ * more possibilities.
+*/
 class Node 
 {
     public $alphabet;
@@ -13,6 +21,12 @@ class Node
         $this->isTerminal = $isTerminal;
     }
 
+    function addChild(Node $node) {
+        if(! $this->hasChild($node->alphabet)) {
+            $this->children[$node->alphabet] = $node;
+        }
+    }
+    
     function hasChild(string $alphabet): bool {
         return isset($this->children[$alphabet]);
     }
@@ -33,9 +47,5 @@ class Node
         return $this->children;
     }
 
-    function addChild(Node $node) {
-        if(! $this->hasChild($node->alphabet)) {
-            $this->children[$node->alphabet] = $node;
-        }
-    }
+
 }
--- src/DataStructures/Trie/Trie.php
+++ src/DataStructures/Trie/Trie.php
@@ -1,6 +1,14 @@
 <?php
 namespace App\DataStructures\Trie;
 
+/**
+ * Explanation
+ * -----------------
+ * The Trie class will have a default root node that does not contain any alphabet.
+ * All alphabets will be contained from the children hashmap
+ * Hence in a full dictionary the $this->root->alphabet would be null
+ * and children would be a hashMap with all alphabets as keys
+*/
 class Trie
 {
     protected $root;
@@ -10,6 +18,10 @@ class Trie
         $this->root = new Node(null, false);
     }
 
+    /**
+     * This will add any string as a word to the trie and mark the last node (alphabet as isTerminal=true) to indicate the end of the word
+     * If part of the word already exists, it will simply continue till it ends up storing the entire word in the tree.
+    */
     function addWord(string $word) {
         $chars = str_split(strrev($word));
         $node = $this->root;
@@ -30,6 +42,9 @@ class Trie
         }
     }
 
+    /**
+     * Will check if the given word is a full word in the dictionary ( trie )
+    */
     function isWord(string $word): bool {
         $chars = str_split(strrev($word));
 
@@ -52,6 +67,9 @@ class Trie
         return $isWord;
     }
 
+    /**
+     * This will take a prefix and send a list of possible words with that prefix.
+    */
     function getSuggestions(string $prefix): array {
         $chars = str_split(strrev($prefix));
         $node = $this->root;
